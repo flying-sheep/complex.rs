64c12f76353bfd509391ec77cb5e319ebd4771d4
Jorge Aparicio <japaricious@gmail.com>
2015-01-03 06:47:50 +0000

    docs!

---
+++ implementors/core/kinds/trait.Copy.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['complex'] = ["<a class='stability Experimental' title='Experimental'></a>impl&lt;T: <a class='trait' href='http://doc.rust-lang.org/nightly/core/kinds/trait.Copy.html' title='core::kinds::Copy'>Copy</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/kinds/trait.Copy.html' title='core::kinds::Copy'>Copy</a> for <a class='struct' href='complex/struct.Complex.html' title='complex::Complex'>Complex</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/ops/trait.Sub.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['complex'] = ["<a class='stability Experimental' title='Experimental'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Sub.html' title='core::ops::Sub'>Sub</a>&lt;T, <a class='struct' href='complex/struct.Complex.html' title='complex::Complex'>Complex</a>&lt;T&gt;&gt; for <a class='struct' href='complex/struct.Complex.html' title='complex::Complex'>Complex</a>&lt;T&gt;","<a class='stability Experimental' title='Experimental'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Sub.html' title='core::ops::Sub'>Sub</a>&lt;<a class='struct' href='complex/struct.Complex.html' title='complex::Complex'>Complex</a>&lt;T&gt;, <a class='struct' href='complex/struct.Complex.html' title='complex::Complex'>Complex</a>&lt;T&gt;&gt; for <a class='struct' href='complex/struct.Complex.html' title='complex::Complex'>Complex</a>&lt;T&gt;","<a class='stability Experimental' title='Experimental'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Sub.html' title='core::ops::Sub'>Sub</a>&lt;<a class='struct' href='complex/struct.Complex.html' title='complex::Complex'>Complex</a>&lt;T&gt;, <a class='struct' href='complex/struct.Complex.html' title='complex::Complex'>Complex</a>&lt;T&gt;&gt; for T",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
